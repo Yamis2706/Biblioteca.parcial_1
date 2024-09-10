cd5dde105ae4a53fc9e926fabb01e35083fa1e22
Yamileth Londoño Burgos <142650446+Yamis2706@users.noreply.github.com>
2024-09-10 09:43:45 -0500

    Creación de Clases
    Se crean las clases necesarias para el desarrollo del proyecto

---
+++ co.edu.uniquindio.biblioteca.parcial_1/src/main/java/co/edu/uniquindio/biblioteca/parcial_1/model/Jornalero.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.biblioteca.parcial_1.model;
+
+public class Jornalero {
+}
---
+++ co.edu.uniquindio.biblioteca.parcial_1/src/main/java/co/edu/uniquindio/biblioteca/parcial_1/model/enumeracion/TipoContrato.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.biblioteca.parcial_1.model.enumeracion;
+
+public enum TipoContrato {
+}
